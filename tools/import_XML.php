4d83ffb335650f089c780b09525398b55c29846e
lkorczewski <lukasz.korczewski@gmail.com>
2013-10-27 21:09:17 +0100

    XML importing

---
+++ tools/import_XML.php
@@ -0,0 +1,30 @@
+#!/usr/bin/php
+<?php
+
+//====================================================
+// initialization
+//====================================================
+
+require_once '../include/script.php';
+
+Script::set_root_path('..');
+$config = Script::load_config();
+
+require_once 'database/database.php';
+require_once 'dictionary/mysql_data.php';
+require_once 'dictionary/import/XML_importer.php';
+
+$database = Script::connect_to_database();
+
+$data = new MySQL_Data($database);
+
+//====================================================
+// parsing
+//====================================================
+
+$XML_file = $argv[1];
+
+$importer = new Dictionary\XML_Importer($data);
+$importer->parse($XML_file);
+
+?>
