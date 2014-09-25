3eddc9b870a601e89f54737468a59a47e636d027
Łukasz Korczewski <lukasz.korczewski@gmail.com>
2014-09-25 18:07:49 +0200

    bugfix: wrong message when not authorized

--- public/atomics/_authorized_header.php
+++ public/atomics/_authorized_header.php
@@ -5,6 +5,6 @@ use \DCMS\JSON_Response;
 require __DIR__ . '/../../bootstrap.php';
 
 if(!$services->get('session')->get('editor')){
-	$services->get('json_response')->fail(JSON_Response::MESSAGE_NO_PARAMETER);
+	$services->get('json_response')->fail(JSON_Response::MESSAGE_NO_AUTHORIZATION);
 }
 
