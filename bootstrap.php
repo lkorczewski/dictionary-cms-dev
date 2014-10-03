4616eceea68d45d91d96e8451f8cff9b52974aa0
Łukasz Korczewski <lukasz.korczewski@gmail.com>
2014-10-03 10:48:52 +0200

    cleaned todo comment

--- bootstrap.php
+++ bootstrap.php
@@ -8,8 +8,6 @@ $config = require __DIR__ . '/config.php';
 //----------------------------------------------------
 // autoloader
 //----------------------------------------------------
-// todo: path should be based on config!
-
 require_once __DIR__ . '/' .$config['include_path'] . '/core/autoloader.php';
 \Core\Autoloader::register(__DIR__ . '/' . $config['include_path']);
 
