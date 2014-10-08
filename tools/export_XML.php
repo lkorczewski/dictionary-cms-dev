454286525244e6b384c32e12e3d550a4fd7f4954
Łukasz Korczewski <lukasz.korczewski@gmail.com>
2014-10-08 18:17:24 +0200

    bugfix: relative path to bootstrap.php in export_XML.php

--- tools/export_XML.php
+++ tools/export_XML.php
@@ -7,7 +7,7 @@ use Dictionary\XML_Layout;
 // initialization
 //====================================================
 
-require 'bootstrap.php';
+require __DIR__ . '/../bootstrap.php';
 
 require_once __DIR__ . '/../include/script.php';
 
